2bd054ae3d484092918f449cf186606415dc14f1
Tyler Matchett <tyler@pixelget.net>
2016-06-10 20:16:52 -0300

    starting playa shit

---
+++ Assets/Scripts/Data/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+[System.Serializable]
+public class Player {
+	int id;
+	InputDevice device;
+	bool aicontrolled = false;
+	bool ready = false;
+
+	public Player(int id, InputDevice device) {
+		// Load in the weapon and abilities data
+		this.id = id;
+		this.device = device;
+	}
+	
+	// Getters
+	public int ID {
+		get { return id; }
+	}
+	public InputDevice Device {
+		get { return device; }
+	}
+	public bool AIControlled {
+		get { return aicontrolled; }
+	}
+	public bool Ready {
+		get { return ready; }
+	}
+	
+	// Ready Selection Save
+	public void IsReady() {
+		ready = true;
+	}
+	public void NotReady() {
+		ready = false;
+	}
+}
