97a52fb9900c8213fa6a60cb3adecd9e10f7147e
Nicholas Polchies <npolchies@yahoo.com>
2016-06-10 19:38:32 -0300

    Added incomplete CharacterController script

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+//using incontroll;
+
+public class CharacterController : MonoBehaviour {
+	public float health;
+	public float moveSpeed;
+	public float kickDamage;
+	public float punchDamage;
+	public float blockReduction;
+
+	public enum characterState
+	{
+		idle,
+		moving,
+    blocking,
+    punching,
+    kicking
+	};
+
+	//private ActionController;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
