0eaa75ab6f1e064f356ddbd38927b56822399a7e
NickPolchies <polchiesnicholas@gmail.com>
2016-06-11 14:06:19 -0300

    Added debug mode for players. Press S, D, X, C to use Split punch into fast and slow

--- Assets/Scripts/ActionController.cs
+++ Assets/Scripts/ActionController.cs
@@ -6,6 +6,7 @@ public class ActionController : MonoBehaviour
 	public Animator anim;
 	public CharacterManager characterManager;
 	public System.Action OnAnimationEnd;
+	public float blockAnimTimer;
 
 	void OnTriggerEnter2D(Collider2D target)
 	{
@@ -24,6 +25,10 @@ public class ActionController : MonoBehaviour
 				else if (characterManager.state == CharacterState.kicking)
 				{
 					ac.ApplyDamage(characterManager.kickDamage);
+				}
+				else if (characterManager.state == CharacterState.heavyPunching)
+				{
+					ac.ApplyDamage(characterManager.heavyPunchDamage);
 				}
 				Debug.Log("found AC");
 			}
@@ -44,49 +49,59 @@ public class ActionController : MonoBehaviour
 	}
 	public void Punch()
 	{
-		if (Random.Range(0, 2) > 0)
-		{
-			anim.Play("FarPunch");
-			Invoke("animationend", 0.75f);
-		}
-		else
-		{
-			anim.Play("NearPunch");
-			Invoke("animationend", 0.75f);
-		}
-
+		anim.speed = 1.5f;
+		anim.Play("FarPunch");
+		Invoke("animationend", 0.75f/anim.speed);
+	}
+	public void HeavyPunch()
+	{
+		anim.speed = 0.75f;
+		anim.Play("NearPunch");
+		Invoke("animationend", 0.75f/anim.speed);
 	}
 	public void Kick()
 	{
 		if (Random.Range(0, 2) > 0)
 		{
 			anim.Play("FarKick");
-			Invoke("animationend", 0.75f);
 		}
 		else
 		{
 			anim.Play("NearKick");
-			Invoke("animationend", 0.75f);
 		}
+		Invoke("animationend", 0.75f / anim.speed);
 
 	}
 	public void Block()
 	{
+		anim.speed = 2.5f;
 		anim.Play("Block");
-		Invoke("animationend", 0.75f);
+		blockAnimTimer = 0;
+		//Invoke("animationend", 0.75f / anim.speed);
 	}
 	public void Jump()
 	{
 		anim.Play("Jump");
-		Invoke("animationend", 0.75f);
+		Invoke("animationend", 0.75f / anim.speed);
 	}
 
 	void Update()
 	{
 		transform.position += new Vector3(characterManager.direction.x, 0f, 0f) * characterManager.moveSpeed * Time.deltaTime;
+
+		if(characterManager.state == CharacterState.blocking)
+		{
+			blockAnimTimer += Time.deltaTime;
+
+			if (blockAnimTimer >= 0.5)
+			{
+//				anim.Play("Blocking");
+			}
+		}
 	}
 	void animationend()
 	{
+		anim.speed = 1f;
 		OnAnimationEnd();
 	}
 }
