4e08c972eee6eb4a3c9864c1a5b5e6bde1f60693
NickPolchies <polchiesnicholas@gmail.com>
2016-06-10 22:09:33 -0300

    Added action lockout on kick/punch Added stun state Added action lockout to stun Added onHit function Added stun times for attacks and getting hit Moved input handling to separate private function Action lockout currently commented, does not function

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -2,13 +2,16 @@
 using System.Collections;
 using InControl;
 
+
 public enum CharacterState
 {
 	idle,
 	moving,
+	ducking,
 	blocking,
 	punching,
-	kicking
+	kicking,
+	stunned
 };
 
 public class CharacterManager : MonoBehaviour
@@ -17,10 +20,14 @@ public class CharacterManager : MonoBehaviour
 	public float kickDamage;
 	public float punchDamage;
 	public float blockReduction;
+	public Vector2 direction;
+	private float stickDeadzone = 0.15f;
 	public CharacterState state = CharacterState.idle;
 	public Player player;
 	private ActionController actionController;
-
+	private float unlockTimer;
+	private bool actionLock;
+	private float punchTime = 0.3f, kickTime = 0.4f, stunTime = 0.2f;
 
 	//private ActionController;
 
@@ -33,29 +40,65 @@ public class CharacterManager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		var inputDevice = player.Device;
-		if (inputDevice.Action1)
+		InputDevice inputDevice = player.Device;
+
+//		if (!actionLock)
+	//	{
+			handleInput(inputDevice);
+		//}
+
+		if(unlockTimer < 0 && actionLock)
 		{
-			state = CharacterState.punching;
-			//actionController
-    }
-		else if(inputDevice.Action2)
-		{
-			state = CharacterState.kicking;
-		}
-		else if (inputDevice.Action3)
-		{
-			state = CharacterState.blocking;
-		}
-/*
-		else if (inputDevice.Action4)
-		{
-			//Secret fourth button
-		}
-//*/
-		if (inputDevice.LeftStickX > 0.3)
-		{
-			state = CharacterState.moving;
+			actionLock = false;
 		}
   }
+
+	private void handleInput(InputDevice inputDevice)
+	{
+		if (inputDevice.Action1 && state <= CharacterState.blocking)
+		{
+			state = CharacterState.punching;
+			actionController.Punch();
+			unlockTimer = punchTime;
+			actionLock = true;
+		}
+		else if (inputDevice.Action2 && state <= CharacterState.blocking)
+		{
+			state = CharacterState.kicking;
+			actionController.Kick();
+			unlockTimer = kickTime;
+			actionLock = true;
+		}
+		else if (inputDevice.Action3 && state <= CharacterState.blocking)
+		{
+			state = CharacterState.blocking;
+			actionController.Block();
+		}
+		/*
+				else if (inputDevice.Action4)
+				{
+					//Secret fourth button
+				}
+		//*/
+		else if (state <= CharacterState.moving)
+		{
+			if (Mathf.Abs(inputDevice.LeftStickX) > stickDeadzone)
+			{
+				state = CharacterState.moving;
+				direction = inputDevice.Direction.Vector.normalized;
+			}
+			else
+			{
+				state = CharacterState.idle;
+			}
+		}
+	}
+
+	public void onHit()
+	{
+		state = CharacterState.stunned;
+		unlockTimer = stunTime;
+		actionLock = true;
+		//TODO start stun animation
+	}
 }
