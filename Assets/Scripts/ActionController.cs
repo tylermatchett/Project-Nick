56e44823246dbd6f66c30f4bd766f837d0df5ce7
NickPolchies <polchiesnicholas@gmail.com>
2016-06-11 03:22:03 -0300

    Consistent indentation

--- Assets/Scripts/ActionController.cs
+++ Assets/Scripts/ActionController.cs
@@ -1,85 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ActionController : MonoBehaviour {
-
-    public Animator anim;
-    public CharacterManager characterManager;
+public class ActionController : MonoBehaviour
+{
+	public Animator anim;
+	public CharacterManager characterManager;
 	public System.Action OnAnimationEnd;
 
 	void OnTriggerEnter2D(Collider2D target)
-    {
+	{
 		Debug.Log("Hit player");
-        if(target.gameObject.tag == "PlayerHitBox") {
+		if (target.gameObject.tag == "PlayerHitBox")
+		{
 			ActionController ac = target.gameObject.transform.root.GetComponent<ActionController>();
 
 			Debug.Log("Found hit area | ac:  " + ac);
-			if ( ac != null ) {
-				if ( characterManager.state == CharacterState.punching ) {
+			if (ac != null)
+			{
+				if (characterManager.state == CharacterState.punching)
+				{
 					ac.ApplyDamage(characterManager.punchDamage);
-				} else if ( characterManager.state == CharacterState.kicking ) {
+				}
+				else if (characterManager.state == CharacterState.kicking)
+				{
 					ac.ApplyDamage(characterManager.kickDamage);
 				}
 				Debug.Log("found AC");
 			}
-        }
-    }
-    public void ApplyDamage(float damage)
-    {
+		}
+	}
+	public void ApplyDamage(float damage)
+	{
 		Debug.Log("I'm hit broski");
-        if(characterManager.state == CharacterState.blocking)
-        {
-            characterManager.player.Health -= damage * characterManager.blockReduction;
-        }
-        else
-        {
-            characterManager.player.Health -= damage;
-        }
-        
-    }
-    public void Punch()
-    {
-        if(Random.Range(0,2)>0)
-        {
-            anim.Play("FarPunch");
+		if (characterManager.state == CharacterState.blocking)
+		{
+			characterManager.player.Health -= damage * characterManager.blockReduction;
+		}
+		else
+		{
+			characterManager.player.Health -= damage;
+		}
+
+	}
+	public void Punch()
+	{
+		if (Random.Range(0, 2) > 0)
+		{
+			anim.Play("FarPunch");
 			Invoke("animationend", 0.75f);
 		}
-        else
-        {
-            anim.Play("NearPunch");
+		else
+		{
+			anim.Play("NearPunch");
 			Invoke("animationend", 0.75f);
 		}
-   
-    }
-    public void Kick()
-    {
-        if(Random.Range(0,2)>0)
-        {
-            anim.Play("FarKick");
+
+	}
+	public void Kick()
+	{
+		if (Random.Range(0, 2) > 0)
+		{
+			anim.Play("FarKick");
 			Invoke("animationend", 0.75f);
 		}
-        else
-        {
-            anim.Play("NearKick");
+		else
+		{
+			anim.Play("NearKick");
 			Invoke("animationend", 0.75f);
 		}
-       
-    }
-    public void Block()
-    {
-        anim.Play("Block");
+
+	}
+	public void Block()
+	{
+		anim.Play("Block");
 		Invoke("animationend", 0.75f);
 	}
-    public void Jump()
-    {
-        anim.Play("Jump");
+	public void Jump()
+	{
+		anim.Play("Jump");
 		Invoke("animationend", 0.75f);
-    }
+	}
 
-	void Update() {
+	void Update()
+	{
 		transform.position += new Vector3(characterManager.direction.x, 0f, 0f) * characterManager.moveSpeed * Time.deltaTime;
 	}
-	void animationend() {
+	void animationend()
+	{
 		OnAnimationEnd();
 	}
 }
